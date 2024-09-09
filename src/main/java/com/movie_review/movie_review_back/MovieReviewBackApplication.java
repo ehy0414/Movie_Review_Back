b51458020c0f8db72f0d0599621acf2e67171c15
ehy0414 <djagusdyd78@gmail.com>
2024-09-09 10:29:55 +0900

    first commit

---
+++ src/main/java/com/movie_review/movie_review_back/MovieReviewBackApplication.java
@@ -0,0 +1,13 @@
+package com.movie_review.movie_review_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie_review/movie_review_back/MovieReviewBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie_review.movie_review_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
